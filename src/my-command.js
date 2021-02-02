7c3ba5d5fd89ef3dce0381b2f6f7705e9f178f81
Matteo Gratton <icona79@gmail.com>
2021-02-02 18:02:02 +0000

    add items to the webview and add the light/dark theme automatically

--- src/my-command.js
+++ src/my-command.js
@@ -4,44 +4,44 @@ import UI from 'sketch/ui'
 
 const webviewIdentifier = 'prettybuttons.webview'
 
-export default function () {
-  const options = {
-    identifier: webviewIdentifier,
-    width: 240,
-    height: 180,
-    show: false
-  }
+export default function() {
+    const options = {
+        identifier: webviewIdentifier,
+        width: 400,
+        height: 400,
+        show: false
+    }
 
-  const browserWindow = new BrowserWindow(options)
+    const browserWindow = new BrowserWindow(options)
 
-  // only show the window when the page has loaded to avoid a white flash
-  browserWindow.once('ready-to-show', () => {
-    browserWindow.show()
-  })
+    // only show the window when the page has loaded to avoid a white flash
+    browserWindow.once('ready-to-show', () => {
+        browserWindow.show()
+    })
 
-  const webContents = browserWindow.webContents
+    const webContents = browserWindow.webContents
 
-  // print a message when the page loads
-  webContents.on('did-finish-load', () => {
-    UI.message('UI loaded!')
-  })
+    // print a message when the page loads
+    webContents.on('did-finish-load', () => {
+        UI.message('UI loaded!')
+    })
 
-  // add a handler for a call from web content's javascript
-  webContents.on('nativeLog', s => {
-    UI.message(s)
-    webContents
-      .executeJavaScript(`setRandomNumber(${Math.random()})`)
-      .catch(console.error)
-  })
+    // add a handler for a call from web content's javascript
+    webContents.on('nativeLog', s => {
+        UI.message(s)
+        webContents
+            .executeJavaScript(`setRandomNumber(${Math.random()})`)
+            .catch(console.error)
+    })
 
-  browserWindow.loadURL(require('../resources/webview.html'))
+    browserWindow.loadURL(require('../resources/webview.html'))
 }
 
 // When the plugin is shutdown by Sketch (for example when the user disable the plugin)
 // we need to close the webview if it's open
 export function onShutdown() {
-  const existingWebview = getWebview(webviewIdentifier)
-  if (existingWebview) {
-    existingWebview.close()
-  }
+    const existingWebview = getWebview(webviewIdentifier)
+    if (existingWebview) {
+        existingWebview.close()
+    }
 }
