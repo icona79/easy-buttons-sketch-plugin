a5fd21cab4c531e92ebcee092daabc51900c2324
Matteo Gratton <icona79@gmail.com>
2021-02-05 20:11:34 +0000

    add controls for buttons

--- resources/webview.js
+++ resources/webview.js
@@ -8,20 +8,59 @@ document.addEventListener('contextmenu', (e) => {
 //   window.postMessage('nativeLog', 'Called from the webview')
 // })
 
-document.getElementById('parametersSubmit').addEventListener('click', () => {
-    const inputText = document.getElementById('inputText').value;
+// ************************************************** //
+// Validate the input fields                          //
+// ************************************************** //
+const numbersOnly = /^\d+$/;
+const decimalOnly = /^\s*-?[1-9]\d*(\.\d{1,2})?\s*$/;
+const uppercaseOnly = /^[A-Z]+$/;
+const lowercaseOnly = /^[a-z]+$/;
+const stringOnly = /^[A-Za-z0-9]+$/;
 
-    const dropdownValue = document.getElementById("dropdown").value;
+var buttonWidthID = document.getElementById("buttonWidth");
+var buttonHeightID = document.getElementById("buttonHeight");
+var cornerRadiusID = document.getElementById("cornerRadius");
+var cornerRadiusValueID = document.getElementById("cornerRadius");
+
+document.getElementById("parametersSubmit").addEventListener("click", () => {
+    var buttonWidthValue = buttonWidthID.value;
+    var buttonHeightValue = buttonHeightID.value;
+    var cornerRadiusSelection = cornerRadiusID.value;
+    var cornerRadiusValue = 1;
+    if (cornerRadiusSelection === "3") {
+        cornerRadiusValue = cornerRadiusValueID.value;
+    } else if (cornerRadiusSelection === "2") {
+        cornerRadiusValue = buttonHeightID.value / 2;
+    }
 
     var parameters = {
-        inputText: inputText,
-        dropdownValue: dropdownValue,
+        buttonWidth: buttonWidthValue,
+        buttonHeight: buttonHeightValue,
+        cornerRadiusSelection: cornerRadiusSelection,
+        cornerRadiusValue: cornerRadiusValue,
     };
 
     window.postMessage('nativeLog', parameters);
 })
 
 // call the webview from the plugin
-// window.setRandomNumber = (randomNumber) => {
-//   document.getElementById('answer').innerHTML = 'Random number from the plugin: ' + randomNumber
-// }
+// import { f1 } from "../src/my-command.js";
+// f1();
+// console.log("here");
+// document.getElementById("pluginName").innerHTML = title;
+
+// import { hello } from './../src/my-command.js';
+// alert(hello());
+
+// ************************************************** //
+// Expose the value input for corner radius if needed //
+// ************************************************** //
+cornerRadiusID.addEventListener("change", function() {
+    if (cornerRadiusID.value === "3") {
+        document
+            .getElementById("cornerRadiusValue").disabled = false;
+    } else {
+        document
+            .getElementById("cornerRadiusValue").disabled = true;
+    }
+});
--- src/my-command.js
+++ src/my-command.js
@@ -1,8 +1,38 @@
-import BrowserWindow from 'sketch-module-web-view'
-import { getWebview } from 'sketch-module-web-view/remote'
-import UI from 'sketch/ui'
+import BrowserWindow from "sketch-module-web-view";
+import { getWebview } from "sketch-module-web-view/remote";
+// import UI from "sketch/ui";
 
-const webviewIdentifier = 'prettybuttons.webview'
+import { Page } from "sketch/dom";
+import { SymbolMaster } from "sketch/dom";
+
+const webviewIdentifier = "prettybuttons.webview";
+
+var sketch = require("sketch");
+
+// Document variables
+var doc = context.document;
+var document = sketch.getSelectedDocument();
+var artboard = sketch.Artboard;
+// var data = document.sketchObject.documentData();
+// var libraries = sketch.getLibraries();
+// var symbolReferences;
+
+var buttonName = "Buttons/Button";
+var buttonArtboard;
+var xPos = 0;
+var yPos = 0;
+
+var existingSymbols = 0;
+
+const buttonSymbolsPage = "Buttons";
+var page = selectPage(findOrCreatePage(document, buttonSymbolsPage));
+
+var LayerID;
+
+export function hello() {
+    return "Hello World";
+}
+
 
 export default function() {
     /* This is the Webview size */
@@ -10,41 +40,129 @@ export default function() {
         identifier: webviewIdentifier,
         width: 400,
         height: 400,
-        show: false
-    }
+        show: false,
+    };
 
-    const browserWindow = new BrowserWindow(options)
+    const browserWindow = new BrowserWindow(options);
 
     // only show the window when the page has loaded to avoid a white flash
-    browserWindow.once('ready-to-show', () => {
-        browserWindow.show()
-    })
+    browserWindow.once("ready-to-show", () => {
+        browserWindow.show();
+    });
 
-    const webContents = browserWindow.webContents
+    const webContents = browserWindow.webContents;
 
     // print a message when the page loads
 
-    webContents.on('did-finish-load', () => {
-        UI.message('UI loaded!')
-    })
+    // webContents.on("did-finish-load", () => {
+    //     UI.message("UI loaded!");
+    // });
 
     // add a handler for a call from web content's javascript
-    webContents.on('nativeLog', parameters => {
-        console.log(parameters.inputText);
-        console.log(parameters.dropdownValue);
+    webContents.on("nativeLog", parameters => {
 
-        //browserWindow.close();
+        console.log('Configuration: ', parameters);
 
-        // webContents
-        //     .executeJavaScript(`setRandomNumber(${Math.random()})`)
-        //     .catch(console.error)
-    })
+        var buttonWidth = parameters.buttonWidth;
+        var buttonHeight = parameters.buttonHeight;
 
-    browserWindow.loadURL(require('../resources/webview.html'))
+        setSymbolsInPage();
+
+        console.log("X: " + xPos);
+
+        // Create the Artboard which will be the Symbol
+        buttonArtboard = new artboard({
+            name: buttonName,
+            parent: page,
+            frame: {
+                x: xPos,
+                y: yPos,
+                width: buttonWidth,
+                height: buttonHeight,
+            },
+        });
+
+        // var myArtboardLayers = buttonArtboard.layers;
+        // orderLayers(myArtboardLayers);
+
+        // Create the Symbol
+        var mainSymbol = SymbolMaster.fromArtboard(buttonArtboard);
+
+        console.log(mainSymbol);
+
+        document.centerOnLayer(mainSymbol);
+        doc.setZoomValue(75 / 100);
+
+
+        browserWindow.close();
+    });
+
+    browserWindow.loadURL(require("../resources/webview.html"));
 }
 
-// When the plugin is shutdown by Sketch (for example when the user disable the plugin)
-// we need to close the webview if it's open
+// ******************************************************************* //
+// Pages management support functions                                  //
+// ******************************************************************* //
+function findOrCreatePage(document, name) {
+    const [page] = document.pages.filter(page => page.name === name);
+
+    if (!page) {
+        return new Page({
+            name,
+            parent: document,
+        });
+    } else {
+        return page;
+    }
+}
+
+function findOrCreateSymbolPage(document) {
+    const page = Page.getSymbolsPage(document);
+    if (page) {
+        return page;
+    }
+    const symbolsPage = Page.createSymbolsPage();
+    symbolsPage.parent = document;
+    return symbolsPage;
+}
+
+function selectPage(page) {
+    page.selected = true;
+    return page;
+}
+
+function setSymbolsInPage() {
+    symbolsCounter();
+
+    if (existingSymbols > 0) {
+        var lastSymbol = page.layers.slice(-1)[0];
+
+        // console.log(lastSymbol.name);
+
+        var lastSymbolX = lastSymbol.frame.x;
+        var lastSymbolW = lastSymbol.frame.width;
+        // var lastSymbolY = lastSymbol.frame.y;
+        // var lastSymbolH = lastSymbol.frame.height;
+
+        xPos = lastSymbolX + lastSymbolW + 100;
+        //yPos = lastSymbolY + 100;
+
+        //console.log("Counter: " + existingSymbols);
+        buttonName = buttonName + existingSymbols.toString();
+    }
+}
+
+function symbolsCounter() {
+    var currentPage = doc.currentPage();
+    var listOfSymbols = currentPage.symbols();
+
+    existingSymbols = listOfSymbols.count();
+}
+
+// ******************************************************************* //
+// When the plugin is shutdown by Sketch (for example when the user    //
+// disable the plugin) we need to close the webview if it's open       //
+// ******************************************************************* //
 export function onShutdown() {
     const existingWebview = getWebview(webviewIdentifier)
     if (existingWebview) {
