30adf99afe6c361b50a667255091dbe43ee8c3bc
Matteo Gratton <icona79@gmail.com>
2021-02-16 16:39:13 +0000

    UI refined and start the backend work

--- resources/webview.js
+++ resources/webview.js
@@ -1,46 +1,70 @@
 // disable the context menu (eg. the right click menu) to have a more native feel
-document.addEventListener('contextmenu', (e) => {
-    e.preventDefault();
-})
+// document.addEventListener('contextmenu', (e) => {
+//     e.preventDefault();
+// })
 
 // call the plugin from the webview
 // document.getElementById('button').addEventListener('click', () => {
 //     window.postMessage('nativeLog', 'Called from the webview')
 // })
 
-// Validate the input fields                          //
-// ************************************************** //
-const numbersOnly = /^\d+$/;
-const decimalOnly = /^\s*-?[1-9]\d*(\.\d{1,2})?\s*$/;
-const uppercaseOnly = /^[A-Z]+$/;
-const lowercaseOnly = /^[a-z]+$/;
-const stringOnly = /^[A-Za-z0-9]+$/;
-
-var buttonType = 0;
-var buttonType1 = document.getElementById("buttonType1");
-var buttonPaddingHorizontalID = document.getElementById("buttonWidth");
-var buttonHeightID = document.getElementById("buttonHeight");
-var cornerRadiusID = document.getElementById("cornerRadius");
-var cornerRadiusValueID = document.getElementById("cornerRadiusValue");
-var backgroundColorValueID = document.getElementById("backgroundColorValue");
-
+// Enter Key = Click on Create Button button
 document.addEventListener("keyup", function(event) {
     if (event.keyCode === 13) {
         document.getElementById("parametersSubmit").click();
     }
 });
 
+// ************************************************** //
+// Validate the input fields                          //
+// ************************************************** //
+const numbersOnly = "/^\d+$/";
+const decimalOnly = "/^\s*-?[1-9]\d*(\.\d{1,2})?\s*$/";
+const uppercaseOnly = "/^[A-Z]+$/";
+const lowercaseOnly = "/^[a-z]+$/";
+const stringOnly = "/^[A-Za-z0-9]+$/";
+
+// Type
+var buttonType = 0;
+var buttonType1 = document.getElementById("buttonType1");
+// Layout
+var buttonLayout = 0;
+var buttonPaddingHorizontalID = document.getElementById("buttonWidth");
+var buttonHeightID = document.getElementById("buttonHeight");
+var cornerRadiusID = document.getElementById("cornerRadius");
+var cornerRadiusValueID = document.getElementById("cornerRadiusValue");
+// Styles
+var buttonStyle = 0;
+var backgroundColorValueID = document.getElementById("backgroundColorValue");
+var backgroundStyleID = document.getElementById("backgroundStyleValue");
+var textStyleID = document.getElementById("textStyleValue");
+
 document.getElementById("parametersSubmit").addEventListener("click", () => {
+    // Button Type
     var buttonTypeRadios = document.getElementsByName("buttonType");
     for (i = 0; i < buttonTypeRadios.length; i++) {
         if (buttonTypeRadios[i].checked) {
             buttonType = i;
         }
     }
-    var buttonPaddingHorizontalValue = 16;
+
+    // Button Layout
+    var buttonLayoutRadios = document.getElementsByName("buttonLayout");
+    for (i = 0; i < buttonLayoutRadios.length; i++) {
+        if (buttonLayoutRadios[i].checked) {
+            buttonLayout = i;
+        }
+    }
+
+    if (buttonLayout === 0) {
+        var buttonPaddingHorizontalValue = 16;
+    } else {
+        var buttonPaddingHorizontalValue = 96;
+    }
     if (buttonPaddingHorizontalID.value != "") {
         buttonPaddingHorizontalValue = buttonPaddingHorizontalID.value;
     }
+
     var buttonHeightValue = 40;
     if (buttonHeightID.value != "") {
         buttonHeightValue = buttonHeightID.value;
@@ -56,20 +80,39 @@ document.getElementById("parametersSubmit").addEventListener("click", () => {
     } else if (cornerRadiusSelection === "2") {
         cornerRadiusValue = buttonHeightID.value / 2;
     }
-    var backgroundColorValue = "ffffff"
-    if (backgroundColorValueID.value != "") {
-        backgroundColorValue = backgroundColorValueID.value;
+
+    // Button Style
+    var buttonStyleRadios = document.getElementsByName("buttonStyle");
+    for (i = 0; i < buttonStyleRadios.length; i++) {
+        if (buttonStyleRadios[i].checked) {
+            buttonStyle = i;
+        }
     }
+    if (buttonStyle === 0) {
+        var backgroundStyle = backgroundStyleID.value;
+        var textStyle = ""; //textStyleID.value;
+    } else {
+        var backgroundColorValue = "ffffff";
+        if (backgroundColorValueID.value != "") {
+            backgroundColorValue = backgroundColorValueID.value;
+        }
+    }
+
     var parameters = {
         buttonType: buttonType,
+        buttonLayout: buttonLayout,
         buttonPaddingHorizontalValue: buttonPaddingHorizontalValue,
         buttonHeightValue: buttonHeightValue,
         cornerRadiusSelection: cornerRadiusSelection,
         cornerRadiusValue: cornerRadiusValue,
+        backgroundStyle: backgroundStyle,
+        textStyle: textStyle,
         backgroundColorValue: backgroundColorValue,
     };
 
-    window.postMessage('nativeLog', parameters);
+    //console.log(parameters);
+
+    window.postMessage("nativeLog", parameters);
 })
 
 // ************************************************** //
@@ -84,6 +127,42 @@ cornerRadiusID.addEventListener("change", function() {
             .getElementById("cornerRadiusValue").disabled = true;
     }
 });
+
+// ************************************************** //
+// Expose the layout management items                 //
+// ************************************************** //
+if (document.querySelector('input[name="buttonLayout"]')) {
+    document.querySelectorAll('input[name="buttonLayout"]').forEach((elem) => {
+        elem.addEventListener("change", function(event) {
+            var item = event.target.value;
+            if (item === "1") {
+                document.getElementById("smartLayout").hidden = false;
+                document.getElementById("fixedLayout").hidden = true;
+            } else {
+                document.getElementById("smartLayout").hidden = true;
+                document.getElementById("fixedLayout").hidden = false;
+            }
+        });
+    });
+}
+
+// ************************************************** //
+// Expose the right styles management items           //
+// ************************************************** //
+if (document.querySelector('input[name="buttonStyle"]')) {
+    document.querySelectorAll('input[name="buttonStyle"]').forEach((elem) => {
+        elem.addEventListener("change", function(event) {
+            var item = event.target.value;
+            if (item === "1") {
+                document.getElementById("existingStyles").hidden = false;
+                document.getElementById("newStyles").hidden = true;
+            } else {
+                document.getElementById("existingStyles").hidden = true;
+                document.getElementById("newStyles").hidden = false;
+            }
+        });
+    });
+}
 
 // Function to populate the Styles dropdown
 window.fillStylesDropdown = function(stylesArray) {
